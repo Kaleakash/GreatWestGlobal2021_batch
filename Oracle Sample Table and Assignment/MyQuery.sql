d9f300eaea1b879ca7a86ee91174d5ceadd84761
akash <akash300383@gmail.com>
2021-09-08 15:28:09 +0530

    done

--- Oracle Sample Table and Assignment/MyQuery.sql
+++ Oracle Sample Table and Assignment/MyQuery.sql
@@ -109,4 +109,33 @@ select * from employees;
 select emp1.first_name,emp1.job_id,emp2.first_name from employees emp1, employees emp2 where emp1.manager_id= emp2.employee_id;
 select * from jobs;
 
+select first_name,upper(first_name) from employees;
 
+select * from dual;
+select upper('raj'), lower('RAJ'), initcap('raj') from dual;
+select substr('rajdeep',2),substr('rajdeep',2,5) from dual;
+select length('raj deep') from dual;
+
+select round(345.678,0) from dual;
+select round(345.678,1) from dual;
+select round(345.678,2) from dual;
+select round(345.678,3) from dual;
+select round(344.678,-1) from dual;
+select round(344.678,-2) from dual;
+select round(644.678,-3) from dual;
+
+select trunc(345.678,0) from dual;
+select trunc(345.678,1) from dual;
+select trunc(345.678,2) from dual;
+select trunc(345.678,3) from dual;
+select trunc(344.678,-1) from dual;
+select trunc(344.678,-2) from dual;
+select trunc(644.678,-3) from dual;
+
+select ceil(10.1), ceil(10.9), floor(10.1), floor(10.9) from dual;
+
+select sysdate from dual;
+select add_months(sysdate, 2) from dual;
+select next_day(sysdate,'Mon') from dual;
+
+select months_between(sysdate, '01-jan-21') from dual;
